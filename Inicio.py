357965bcbfe6d0e377d5aff620ed03a82d6df150
Jesus Dominguez <dominguezreyesjesus142014@gmail.com>
2021-04-03 22:25:31 -0600

    03 de Marzo de 2021

--- Inicio.py
+++ Inicio.py
@@ -20,9 +20,13 @@ def addImg():
     global imagenes
     #Pedir nombre del archivo
     nom_img = filedialog.askopenfilename(title="Seleccione archivo",filetypes=(("jpeg files",".jpg"),("png files",".png"),("all files",".*")))
-    img = cv2.imread(nom_img)
-    imagenes = append(ImageTk.PhotoImage(Image.fromarray(img)),imagenes)
-    label = Label(frame,image=imagenes[0],width=100,height=150)
+    #Las siguientes 2 líneas utilizan PIL
+    img = Image.open(nom_img)
+    #img.size[0] #Esto es el número de columnas
+    #img.size[1] #Esto es el número de filas
+    imagenes = append(ImageTk.PhotoImage(file=nom_img),imagenes)
+    #label = Label(frame,image=imagenes[0],width=100,height=150)
+    label = Label(frame,image=imagenes[0],width=img.size[0],height=img.size[1])
     label.place(x=100,y=100)
     label.bind("<Button-1>",drag_start)
     label.bind("<B1-Motion>",drag_motion)
@@ -32,16 +36,18 @@ root=Tk()
 root.title("Ventana de pruebas")
 #root.config(bd=15,cursor="hand2")
 root.geometry("900x700")
+root.config(bd=20)
+
 w=700
 h=500
 x=w/2
 y=h/2
 imagenes = array(0)
 
-frame=Frame(root,height=h,width=w,bg="gray")
-frame.pack(pady=50)
+bAddImg=Button(root,text="Abrir Imagen",command=lambda:addImg(),font=(18))
+bAddImg.pack(pady=10)
 
-bAddImg=Button(root,text="Añadir Imagen",command=lambda:addImg(),font=(18))
-bAddImg.pack()
+frame=Frame(root,height=h,width=w,bg="gray")
+frame.pack(pady=20,fill='both',expand=1)
 
 root.mainloop()
--- Inicio2.py
+++ Inicio2.py
@@ -1,4 +1,8 @@
 from tkinter import *
+from tkinter import filedialog
+import cv2
+from PIL import Image,ImageTk
+from numpy import *
 
 def drag_start(event):
     widget = event.widget
@@ -11,19 +15,34 @@ def drag_motion(event):
     y = widget.winfo_y() - widget.startY + event.y
     widget.place(x=x,y=y)
 
+#Funcion con la que se pide una imagen al usuario
+def addImg():
+    global imagenes
+    #Pedir nombre del archivo
+    nom_img = filedialog.askopenfilename(title="Seleccione archivo",filetypes=(("jpeg files",".jpg"),("png files",".png"),("all files",".*")))
+    #Las siguientes 2 líneas utilizan cv2
+    img = cv2.imread(nom_img)
+    imagenes = append(ImageTk.PhotoImage(Image.fromarray(img)),imagenes)
+    label = Label(frame,image=imagenes[0],width=100,height=150)
+    label.place(x=100,y=100)
+    label.bind("<Button-1>",drag_start)
+    label.bind("<B1-Motion>",drag_motion)
 
-window = Tk()
+#Raíz
+root=Tk()
+root.title("Ventana de pruebas")
+#root.config(bd=15,cursor="hand2")
+root.geometry("900x700")
+w=700
+h=500
+x=w/2
+y=h/2
+imagenes = array(0)
 
-label = Label(window,bg="red",width=10,height=5)
-label.place(x=0,y=0)
+frame=Frame(root,height=h,width=w,bg="gray")
+frame.pack(pady=50)
 
-label2 = Label(window,bg="blue",width=10,height=5)
-label2.place(x=100,y=100)
+bAddImg=Button(root,text="Añadir Imagen",command=lambda:addImg(),font=(18))
+bAddImg.pack()
 
-label.bind("<Button-1>",drag_start)
-label.bind("<B1-Motion>",drag_motion)
-
-label2.bind("<Button-1>",drag_start)
-label2.bind("<B1-Motion>",drag_motion)
-
-window.mainloop()
+root.mainloop()
